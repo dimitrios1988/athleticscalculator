1662e94c2a05ba7055beaf40679f14f8ec3e2155
Dimitrios Chondrokoukis <dimitrios1988@msn.com>
2019-06-14 11:48:08 +0300

    Changed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'ranking', pathMatch: 'full' },
-  { path: 'ranking', loadChildren: './ranking/ranking.module#RankingPageModule' },
+  // { path: '', redirectTo: 'ranking', pathMatch: 'full' },
+  // { path: 'ranking', loadChildren: './ranking/ranking.module#RankingPageModule' },  
+  { path: '', loadChildren: './ranking/ranking.module#RankingPageModule' },
+  { path: 'ranking', redirectTo: '', pathMatch: 'full' },
   { path: 'performances', loadChildren: './performances/performances.module#PerformancesPageModule' },
   { path: 'info', loadChildren: './info/info.module#InfoPageModule' },
   { path: 'meetings', loadChildren: './meetings/meetings.module#MeetingsPageModule' },
