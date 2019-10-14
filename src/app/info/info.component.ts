c6620cc9b7e13a3026259d01fdc2fc5f855df5a7
Dimitrios Chondrokoukis <dimitrios1988@msn.com>
2019-10-14 09:33:22 +0300

    Added card

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -13,11 +13,15 @@ export class InfoComponent implements OnInit {
   }
 
   onRankingTables() {
-    window.open("https://www.iaaf.org/world-ranking-rules/basics", "_blank");
+    window.open('https://www.iaaf.org/world-ranking-rules/basics', '_blank');
   }
 
   onScoringTables() {
-    window.open("https://www.iaaf.org/news/iaaf-news/scoring-tables-2017", "_blank");
+    window.open('https://www.iaaf.org/news/iaaf-news/scoring-tables-2017', '_blank');
+  }
+
+  onContact() {
+    window.open('mailto:dimi.chondrokoukis@gmail.com?Subject=Athletics%20Ranking%20Points%20Calculator', '_blank');
   }
 
 }
