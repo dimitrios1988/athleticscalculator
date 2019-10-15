dc1510c4da8bcf5c7e5ca24a520d0ac36075cd21
Dimitrios Chondrokoukis <dimitrios1988@msn.com>
2019-10-15 16:31:26 +0300

    Refactoring

--- src/app/pwa/pwa.service.ts
+++ src/app/pwa/pwa.service.ts
@@ -12,7 +12,6 @@ export class PwaService {
     swUpdate.available.subscribe(event => {
       this.openSnackBar().afterDismissed().subscribe(() => window.location.reload());
     });
-
   }
 
   private openSnackBar() {
