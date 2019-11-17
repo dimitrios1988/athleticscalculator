b73bd014344a1e0a2cd6654e2b152a652d6d86da
Dimitrios Chondrokoukis <dimitrios1988@msn.com>
2019-11-18 00:20:19 +0200

    Removed unnecessary language locales from bundle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -77,20 +77,11 @@ import { registerLocaleData } from '@angular/common';
 })
 export class AppModule {
   constructor() {
-    import(`@angular/common/locales/${getLocale().substring(0, getLocale().indexOf('-'))}.js`).then(lang => registerLocaleData(lang.default));
+    import(
+      /* webpackInclude: /(el|en|it|de|ru|es)\.js$/ */
+      `@angular/common/locales/${getLocale().substring(0, getLocale().indexOf('-'))}.js`).then(lang => registerLocaleData(lang.default));
   }
 }
-
-/* export function getLocale() {
-  
-  var language;
-  if (window.navigator.languages) {
-    language = window.navigator.languages[0];
-  } else {
-    language = window.navigator.language;
-  }
-  return language;
-} */
 
 export function getLocale(): string {
   const defaultValue = 'el-GR'
